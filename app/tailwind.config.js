c0454021b83a07c069759cb109594c0bf95b968d
Enache Adelina <enache.adelina99@gmail.com>
2024-04-30 01:09:04 +0300

    Add Inter font

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   // NOTE: Update this to include the paths to all of your component files.
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
@@ -13,6 +15,33 @@ module.exports = {
     extend: {
       borderRadius: {
         sm: "5px",
+      },
+      fontFamily: {
+        inter: ["Inter", "system-ui", "sans-serif"],
+        "inter-thin": ["Inter_100Thin", "Inter", "system-ui", "sans-serif"],
+        "inter-extra-light": [
+          "Inter_200ExtraLight",
+          "Inter",
+          "system-ui",
+          "sans-serif",
+        ],
+        "inter-light": ["Inter_300Light", "Inter", "system-ui", "sans-serif"],
+        "inter-normal": ["Inter_400Normal", "Inter", "system-ui", "sans-serif"],
+        "inter-medium": ["Inter_500Medium", "Inter", "system-ui", "sans-serif"],
+        "inter-semi-bold": [
+          "Inter_600SemiBold",
+          "Inter",
+          "system-ui",
+          "sans-serif",
+        ],
+        "inter-bold": ["Inter_700Bold", "Inter", "system-ui", "sans-serif"],
+        "inter-extra-bold": [
+          "Inter_800ExtraBold",
+          "Inter",
+          "system-ui",
+          "sans-serif",
+        ],
+        "inter-black": ["Inter_900Black", "Inter", "system-ui", "sans-serif"],
       },
     },
   },
