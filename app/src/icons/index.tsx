d64a442c562c405b097a4e738637d9216f57919d
Enache Adelina <enache.adelina99@gmail.com>
2024-04-30 03:07:14 +0300

    Add icons component

---
+++ app/src/icons/index.tsx
@@ -0,0 +1,13 @@
+import { View, ViewProps } from "react-native";
+import { EditIcon } from "./EditIcon";
+
+export type IconName = "edit" | "linkedin";
+export type IconProps = { name: IconName; className: ViewProps["className"] };
+
+export const Icon = ({ name, className }: IconProps) => {
+  return (
+    <View className={`mx-1 ${className}`}>
+      {name == "edit" && <EditIcon />}
+    </View>
+  );
+};
